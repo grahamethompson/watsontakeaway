6813e181bb2b0562bf0877dfa3171a811b0ae0eb
grahame <grahame.thompson@gmail.com>
2017-05-16 19:21:18 +1000

    html min

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,18 @@ var sourcemaps = require('gulp-sourcemaps');
 var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
+var htmlmin = require('gulp-html-minifier');
 
 //script paths
 var jsFiles = 'js/*.js',
     jsDest = 'dist/scripts',
     distDest ="../dist/";
+
+gulp.task('minify', function() {
+    gulp.src('html/index.html')
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest('.'))
+});
 
 
 gulp.task('minify-css', function() {
@@ -22,7 +29,7 @@ gulp.task('minify-css', function() {
 });
 
 // The default task (called when you run `gulp` from cli)
-gulp.task('default', ['watch', 'scripts', 'images']);
+gulp.task('default', ['minify-css', 'minify']);
 
 
 gulp.task('minify-js', function() {
